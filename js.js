48600fdc66bd9b3dbf5661a8bd91643c7ca7fdce
Divaldo Hélder <divaldohelder08>
2023-06-27 17:24:45 +0100

    Adicionando pasta completa

---
+++ js.js
@@ -0,0 +1,598 @@
+const cartons = [
+    {
+        nome: "Kimetsu no yaiba",
+        type: "Acção",
+        img: "images/cartoons/0.jpg",
+        qld: "HD",
+        drc: "2h40",
+        s: 1,
+        e: 2,
+        ctg: "cartons",
+        txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+
+
+    },
+    {
+        nome: "Dragon ball",
+        type: "Acção",
+        img: "images/cartoons/1.jpg",
+        qld: "HD",
+        drc: "2h40",
+        s: 1,
+        e: 2,
+        ctg: "cartons",
+        txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+    },
+    {
+        nome: "Weathering with you",
+        type: "Romance",
+        img: "images/cartoons/2.jpg",
+        qld: "HD",
+        drc: "2h40",
+        s: 1,
+        e: 2,
+        ctg: "cartons",
+        txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+    },
+    {
+        nome: "your name",
+        type: "Romance",
+        img: "images/cartoons/3.jpg",
+        qld: "HD",
+        drc: "2h40",
+        s: 1,
+        e: 2,
+        ctg: "cartons",
+        txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+    },
+    {
+        nome: "Over the Mon",
+        type: "Romance",
+        img: "images/cartoons/4.jpg",
+        qld: "HD",
+        drc: "2h40",
+        s: 1,
+        e: 2,
+        ctg: "cartons",
+        txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+    },
+    {
+        nome: "The Croods a new age",
+        type: "Romance",
+        img: "images/cartoons/5.jpg",
+        qld: "HD",
+        drc: "2h40",
+        s: 1,
+        e: 2,
+        ctg: "cartons",
+        txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+    }
+],
+    movie = [
+        {
+            titulonome: "bat-man",
+            type: "Acção",
+            img: "images/movies/0.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "movie",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "blood-shot",
+            type: "Acção",
+            img: "images/movies/1.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "movie",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "call",
+            type: "Terror",
+            img: "images/movies/2.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "movie",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "captain-marvel",
+            type: "Acção",
+            img: "images/movies/3.png",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "movie",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "end-game",
+            type: "Acção",
+            img: "images/movies/4.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "movie",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "hunter-killer",
+            type: "Acção",
+            img: "images/movies/5.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "movie",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "insidious",
+            type: "Terror",
+            img: "images/movies/6.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "movie",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "love-roise",
+            type: "Romnce",
+            img: "images/movies/7.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "movie",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "resident-evil",
+            type: "Terror",
+            img: "images/movies/8.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "movie",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "theatre-dead",
+            type: "terror",
+            img: "images/movies/9.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "movie",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "transformer",
+            type: "Acção",
+            img: "images/movies/10.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "movie",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+    ],
+    series = [
+        {
+            nome: "mandalorian",
+            type: "Fixão",
+            img: "images/series/0.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "series",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "penthouses",
+            type: "terror",
+            img: "images/series/1.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "series",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "star trek discovery 2",
+            type: "Fixão",
+            img: "images/series/2.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "series",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "star trek discovery 1",
+            type: "Ficção",
+            img: "images/series/3.jpeg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "series",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "stranger-thing",
+            type: "Acção",
+            img: "images/series/4.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "series",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "supergirl",
+            type: "Acção",
+            img: "images/series/5.jpg",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "series",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "the-falcon",
+            type: "Terror",
+            img: "images/series/6.webp",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "series",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "wanda",
+            type: "Romnce",
+            img: "images/series/7.png",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "series",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+        {
+            nome: "wanda 2",
+            type: "Terror",
+            img: "images/series/8.webp",
+            qld: "HD",
+            drc: "2h40",
+            s: 1,
+            e: 2,
+            ctg: "series",
+            txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptatem nisi optio nam voluptates nobis facilis repellendus unde quod."
+        },
+    ]
+const conent = document.querySelector(".conent")
+const div = document.createElement("div")
+let link = document.querySelectorAll("a")
+let p = document.createElement("p")
+let pTxt = document.createTextNode('"Não foram encontrados"')
+p.appendChild(pTxt)
+p.classList.add("erro")
+
+link[1].addEventListener("click", () => {
+    clean()
+    clear()
+    //row()
+    forCard(movie)
+    opecoa(movie)
+    about()
+})
+
+link[2].addEventListener("click", () => {
+    clean()
+    clear()
+    //row()
+    forCard(series)
+    opecoa(series)
+    about()
+})
+
+link[3].addEventListener("click", () => {
+    clean()
+    clear()
+    //row()
+    forCard(cartons)
+    opecoa(cartons)
+    about()
+})
+
+
+function forCard(array) {
+    array.forEach(item => {
+        let card = document.createElement("div"),
+            image = document.createElement("div"),
+            img = document.createElement("img"),
+            cardContent = document.createElement("div"),
+            nameT = document.createTextNode(item.nome),
+            nomeProf = document.createElement("div"),
+            prof = document.createElement("span"),
+            name = document.createElement("span"),
+            divButton = document.createElement("div"),
+            button = document.createElement("button"),
+            profT = document.createTextNode(item.type),
+            buttonT = document.createTextNode("Ver me"),
+            star = document.createElement("i"),
+            starFill = document.createElement("i"),
+            rating = document.createElement("div"),
+            button1 = document.createElement("button"),
+            button1T = document.createTextNode("Sobre mim");
+        card.classList.add("card")
+        image.classList.add("image")
+        cardContent.classList.add("card-content")
+        nomeProf.classList.add("nome-prof")
+        name.classList.add("name")
+        rating.classList.add("rating")
+        starFill.classList.add("bi-star-fill")
+        star.classList.add("bi-star")
+        divButton.classList.add("button")
+        button.classList.add("sobre")
+        prof.classList.add("prof")
+        button1.classList.add("HereMe")
+        img.src = item.img
+        image.appendChild(img)
+        name.appendChild(nameT)
+        prof.appendChild(profT)
+        nomeProf.appendChild(name)
+        nomeProf.appendChild(prof)
+        button.appendChild(buttonT)
+        button1.appendChild(button1T)
+        divButton.appendChild(button)
+        divButton.appendChild(button1)
+        rating.appendChild(starFill)
+        starFill = document.createElement("i")
+        starFill.classList.add("bi-star-fill")
+        rating.appendChild(starFill)
+        starFill = document.createElement("i")
+        starFill.classList.add("bi-star-fill")
+        rating.appendChild(starFill)
+        rating.appendChild(star)
+        star = document.createElement("i")
+        star.classList.add("bi-star")
+        rating.appendChild(star)
+        cardContent.appendChild(image)
+        cardContent.appendChild(nomeProf)
+        cardContent.appendChild(rating)
+        cardContent.appendChild(divButton)
+        card.appendChild(cardContent)
+        let about = document.createElement("div")
+        about.classList.add("about")
+        let aboutHeader = document.createElement("div")
+        aboutHeader.classList.add("about-header")
+        let aboutName = document.createElement("span")
+        aboutName.classList.add("about-name")
+        aboutName.appendChild(document.createTextNode(item.nome))
+        let close = document.createElement("span")
+        close.classList.add("close")
+        close.appendChild(document.createTextNode("x"))
+        aboutHeader.appendChild(aboutName)
+        aboutHeader.appendChild(close)
+
+
+
+
+
+        let aboutStatus = document.createElement("div")
+        aboutStatus.classList.add("status")
+        let span = document.createElement("span")
+        let ul = document.createElement("ul")
+        let li = document.createElement("li")
+        li.appendChild(document.createTextNode(item.type))
+        ul.appendChild(li)
+
+        let li1 = document.createElement("li")
+        let b = document.createElement("b")
+        b.appendChild(document.createTextNode(item.qld))
+        li1.appendChild(b)
+        ul.appendChild(li1)
+
+        let li2 = document.createElement("li")
+        let b1 = document.createElement("b")
+        b1.appendChild(document.createTextNode(item.qld))
+        li2.appendChild(b1)
+        ul.appendChild(li2)
+
+        let li3 = document.createElement("li")
+        let b2 = document.createElement("b")
+        b2.appendChild(document.createTextNode(item.drc))
+        li3.appendChild(b2)
+        ul.appendChild(li3)
+
+        let li4 = document.createElement("li")
+        li4.appendChild(document.createTextNode("S:" + item.s))
+        ul.appendChild(li4)
+
+        let li5 = document.createElement("li")
+        li5.appendChild(document.createTextNode(item.ctg))
+        ul.appendChild(li5)
+
+        let li6 = document.createElement("li")
+        let b3 = document.createElement("b")
+        b3.appendChild(document.createTextNode("E:" + item.e))
+        li6.appendChild(b3)
+        ul.appendChild(li6)
+
+        span.appendChild(ul)
+        aboutStatus.appendChild(span)
+
+
+
+        let inner = document.createElement("p")
+        inner.classList.add("inner")
+        inner.appendChild(document.createTextNode(item.txt))
+
+        about.appendChild(aboutHeader)
+        about.appendChild(aboutStatus)
+        about.appendChild(inner)
+        card.appendChild(about)
+        conent.appendChild(card)
+    });
+}
+
+
+function about() {
+    let HereMe = document.querySelectorAll(".HereMe")
+    let card = document.querySelectorAll(".card")
+    for (let i = 0; i < HereMe.length; i++) {
+        HereMe[i].addEventListener("click", () => {
+            card[i].classList.toggle("flip")
+        })
+    }
+    let close=document.querySelectorAll(".close")
+    for(let i=0;i<close.length;i++){
+        close[i].addEventListener("click",()=>{
+            card[i].classList.remove("flip")
+        })
+    }
+}
+
+function opecoa(array) {
+    let ope2 = document.getElementById("ope")
+    ope2.addEventListener("change", () => {
+        ope2 = document.getElementById("ope").value
+        switch (ope2) {
+            case "1":
+                clean()
+                let novo1 = array.filter(item => { return item.type == "Acção" })
+                clear()
+                if (novo1.length > 0) {
+                    forCard(novo1)
+                    about()
+                }
+                else {
+                    conent.appendChild(p)
+                }
+                break
+            case "2":
+                clean()
+                let novo2 = array.filter(item => { return item.type == "Terror" })
+                clear()
+                if (novo2.length > 0) {
+                    forCard(novo2)
+                    about()
+                } else {
+                    conent.appendChild(p)
+                }
+                break
+            case "3":
+                clean()
+                let novo3 = array.filter(item => { return item.type == "Romance" })
+                clear()
+                if (novo3.length > 0) {
+                    forCard(novo3)
+                    about()
+                }
+                else {
+                    conent.appendChild(p)
+                }
+                break
+            case "4":
+                clean()
+                let novo4 = array.filter(item => { return item.type == "Aventura" })
+                clear()
+                if (novo4 > 0) {
+                    forCard(novo4)
+                    about()
+                } else {
+                    conent.appendChild(p)
+                }
+                break
+            default:
+                clean()
+                clear()
+                forCard(array)
+                about()
+                break
+        }
+    })
+}
+function clean() {
+    if (conent.contains(p)) {
+        conent.removeChild(p)
+    }
+}
+function clear() {
+    let clear = document.querySelectorAll(".card")
+    for (let i = 0; i < clear.length; i++) {
+        clear[i].remove()
+    }
+}
+/*
+function row(){
+    divRow.remove(newspan)
+    let divRow=document.querySelector(".row") 
+    let rowt=document.createTextNode("Confira já os novos lançamentos")
+    let array=["Acção","Terror","Romance","Aventura","Fixão"]
+    let newspan=document.createElement("span")
+    
+    if(divRow.contains(newspan)){
+        divRow.remove(newspan)
+    }
+    let selectROw=document.createElement("select")
+    selectROw.id="ope"
+    let opeRow=document.createElement("option")
+    opeRow.value=""
+    opeRow.innerText="Todos"
+
+    selectROw.appendChild(opeRow)
+    for(let i=1;i<6;i++){
+        let opeRow=document.createElement("option")
+        opeRow.value=(i).toString()
+        opeRow.innerText=array[i-1]
+        selectROw.appendChild(opeRow)
+    }
+    let h2=document.createElement("h2")
+    h2.appendChild(rowt)
+
+    
+    newspan.appendChild(h2)
+    newspan.appendChild(selectROw)
+    divRow.appendChild(newspan)
+
+
+}*/
+/*
+    <div class="row">
+(<h2>Confira já os novos lançamentos</h2>
+<select name="" id="ope">
+    <option value="">Todos</option>
+    <option value="1">Acção</option>
+    <option value="2">Terror</option>
+    <option value="3">Romance</option>
+    <option value="4">Aventura</option>
+    <option value="5">Fixão</option>
+</select>
+</div>)
+*/
+
+
